467ab72d78c7f50611c102de89f862800b19cb6d
Gallaangg <galangthoriq@gmail.com>
2025-10-16 13:26:40 +0700

    Add files via upload

---
+++ Haloooooo Hauraaaaaaaaa/script.js
@@ -0,0 +1,29 @@
+$("#messageState").on("change", (x)) {
+    $(".message").removeClass("openNor").removeClass("closeNor");
+    if ($("#messageState").is(":chicked")) {
+        $("message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+        
+    }
+
+
+
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
